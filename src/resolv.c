9fc759a2d59b26cf657c3a724997135604702324
Huazhou Wang <plause@gmail.com>
2014-10-15 22:18:22 +0800

    Resolve the first address of a hostname

--- src/resolv.c
+++ src/resolv.c
@@ -1,6 +1,26 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 #include "config.h"
 
-int main()
+int main(int argc, char **argv)
 {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s HOSTNAME\n", PACKAGE);
+    return 1;
+  }
+
+  char *hostname = *(argv + 1);
+  struct hostent *hostinfo = gethostbyname(hostname);
+
+  if (hostinfo == NULL) {
+    fprintf(stderr, "Can not resolve %s\n", hostname);
+    return 2;
+  }
+
+  struct in_addr *addr = (struct in_addr *) hostinfo->h_addr;
+
+  fprintf(stdout, "%s\n", inet_ntoa(*addr));
+
   return 0;
 }
