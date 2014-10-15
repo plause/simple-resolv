4b9c05e312077d048c7a815ab90ffee57986a6b5
Huazhou Wang <plause@gmail.com>
2014-10-15 22:54:18 +0800

    Add option to resolve all addresses

--- src/resolv.c
+++ src/resolv.c
@@ -1,16 +1,45 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include "config.h"
 
+static void print_addr(struct in_addr *addr)
+{
+  fprintf(stdout, "%s\n", inet_ntoa(*addr));
+}
+
+static void print_addr_list(struct in_addr **list)
+{
+  struct in_addr **addresses = list;
+
+  for (; addresses && *addresses; addresses++) {
+    print_addr(*addresses);
+  }
+}
+
 int main(int argc, char **argv)
 {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s HOSTNAME\n", PACKAGE);
+  int all = 0;
+  int c;
+
+  while ((c = getopt(argc, argv, "a")) != -1) {
+    switch (c) {
+    case 'a':
+      all = 1;
+      break;
+    case '?':
+    default:
+      return 1;
+    }
+  }
+
+  if (argc - optind != 1) {
+    fprintf(stderr, "Usage: %s [-a] HOSTNAME\n", PACKAGE);
     return 1;
   }
 
-  char *hostname = *(argv + 1);
+  char *hostname = *(argv + optind);
   struct hostent *hostinfo = gethostbyname(hostname);
 
   if (hostinfo == NULL) {
@@ -18,9 +47,11 @@ int main(int argc, char **argv)
     return 2;
   }
 
-  struct in_addr *addr = (struct in_addr *) hostinfo->h_addr;
-
-  fprintf(stdout, "%s\n", inet_ntoa(*addr));
+  if (all) {
+    print_addr_list((struct in_addr **) hostinfo->h_addr_list);
+  } else {
+    print_addr((struct in_addr *) hostinfo->h_addr);
+  }
 
   return 0;
 }
